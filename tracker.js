9145e0c128a5e1588bfe3b3eb4750b9418145c01
Simon Adams <simonadams@gmail.com>
2016-05-16 22:16:08 +0100

    cleaned up whitespace

--- tracker.js
+++ tracker.js
@@ -5,11 +5,12 @@ var Poller = require('./lib/poller');
 
 var config_file = "./conf/tracker.json";
 if (process.argv.length > 2) {
-	config_file = process.argv[2];
+    config_file = process.argv[2];
 }
 var config = JSON.parse(fs.readFileSync(config_file, 'utf8'));
+
 if (config.poll) {
-	var poller = new Poller(config);
+    var poller = new Poller(config);
 }
 
 var webService = new WebService(config.port ? config.port : 3000, config.sqlite_file);
